a32fbe3e4d4f8e25ff83d80c4ef91899681da12b
dmitriy <dchechumaev@mail.ru>
2026-03-15 22:24:15 +0300

    Финал + Запрашиваем разрешения на пост уведомлений

--- app/src/main/java/com/example/serviceslearning/MainActivity.kt
+++ app/src/main/java/com/example/serviceslearning/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.serviceslearning
 
+import android.Manifest
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -14,26 +19,49 @@ class MainActivity : AppCompatActivity() {
         const val SONG_URL = "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/h3vL7veZhrT8ghGqKN0s02D0RFqOGPftD7fFtCga.mp3"
     }
 
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // Если выдали разрешение — запускаем сервис.
+            startMusicService()
+        } else {
+            // Иначе просто покажем ошибку
+            Toast.makeText(this, "Can't start foreground service!", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun startMusicService() {
+        // Создаём intent для запуска сервиса
+        val intent = Intent(this, MusicService::class.java).apply {
+            putExtra("song_url", SONG_URL)
+        }
+
+        // Стартуем сервис
+        ContextCompat.startForegroundService(this, intent)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.startMusicServiceButton).setOnClickListener {
-            // Создаём intent для запуска сервиса
-            val intent = Intent(this, MusicService::class.java).apply {
-                putExtra("song_url", SONG_URL)
+
+            // На версии Android 13 и выше — сначала запросим разрешение
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            } else {
+                // На версиях ниже Android 13 —
+                // можно сразу стартовать сервис.
+                startMusicService()
             }
-
-            // Стартуем сервис
-            startService(intent)
-
         }
 
         findViewById<Button>(R.id.startProgressServiceButton).setOnClickListener {
             // Создаём intent для запуска сервиса
             val intent = Intent(this, ProgressService::class.java)
             // Стартуем сервис
-            startService(intent)
+            ContextCompat.startForegroundService(this, intent)
         }
     }
 
--- app/src/main/java/com/example/serviceslearning/MusicService.kt
+++ app/src/main/java/com/example/serviceslearning/MusicService.kt
@@ -1,15 +1,25 @@
 package com.example.serviceslearning
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.content.pm.ServiceInfo
 import android.media.MediaPlayer
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.ServiceCompat
 
 internal class MusicService : Service() {
 
     private companion object {
         const val LOG_TAG = "MusicService"
+        const val NOTIFICATION_CHANNEL_ID = "music_service_channel"
+        const val SERVICE_NOTIFICATION_ID = 100
     }
 
     // Переменная для хранения MediaPlayer
@@ -24,6 +34,51 @@ internal class MusicService : Service() {
 
         Log.d(LOG_TAG, "onCreate")
         mediaPlayer = MediaPlayer()
+        createNotificationChannel()
+
+        ServiceCompat.startForeground(
+            /* service = */ this,
+            /* id = */ SERVICE_NOTIFICATION_ID,
+            /* notification = */ createNotification(),
+            /* foregroundServiceType = */ getForegroundServiceTypeConstant()
+        )
+
+    }
+
+    private fun getForegroundServiceTypeConstant(): Int {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PLAYBACK
+        } else {
+            0
+        }
+    }
+
+    private fun createNotificationChannel() {
+        // Создание каналов доступно только с Android 8.0
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            return
+        }
+
+        val channel = NotificationChannel(
+            /* id= */ NOTIFICATION_CHANNEL_ID,
+            /* name= */ "Music service",
+            /* importance= */ NotificationManager.IMPORTANCE_DEFAULT
+        )
+        channel.description = "Service for playing music"
+
+        // Регистрируем канал уведомлений
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    private fun createNotification(): Notification {
+        return NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setContentTitle("Music foreground service")
+            .setContentText("Our service is working right now!")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setCategory(NotificationCompat.CATEGORY_SERVICE)
+            .build()
     }
 
     // Освобождение ресурсов
